da7c442f7e964ccd05d1cf1c29b951878d1455b9
Martin <w.martin38@icloud.com>
2018-07-19 14:41:20 +0200

    Add decoder and set a buffer to handle payload

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 /*
-* Primary file for the API
-*
-*
-*/
+ * Primary file for the API
+ *
+ *
+ */
 
 // Depedencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all request with a string
-var server = new http.createServer(function(req, res) {
-    
+var server = new http.createServer(function (req, res) {
+
     // Get the URL and parse it
     var parsedUrl = url.parse(req.url, true)
-    
+
     // Get the path from that url
     var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\+|\/+$/g, '')
+    var trimmedPath = path.replace(/^\+|\/+$/g, '');
 
-    // Send the response
-    
-    res.end("Hello world\n");
-    
-    // Log the request path
-    console.log('Request received on this path : ' + trimmedPath)
-    
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+    // Get the HTTP method
+    var method = req
+        .method
+        .toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
+
+    // Get the payload, if any Send the response
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function (data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function () {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end("Hello world\n");
+
+        // Log the headers
+        console.log('Request received with this payload : ', buffer);
+    });
+
 });
 
 // Start the server and listening on port 3000
-server.listen(3000, function(){
+server.listen(3000, function () {
     console.log('The server is listening on port 3000');
 });
